4c320df12b693b9be94a60234454669a359b642f
treykris <trey10027@gmail.com>
2019-08-02 09:16:21 -0400

    removed some assertions

--- test/token_generator.test.js
+++ test/token_generator.test.js
@@ -1,26 +1,25 @@
 const jwt = require('jsonwebtoken');
+const config = require('../src/config');
 const tokenGenerator = require('../src/token_generator');
 
 describe('#tokenGenerator', () => {
   it('generates a new token', () => {
     const token = tokenGenerator();
-    const decoded = jwt.decode(token.token, {complete: true});
+    const decoded = jwt.decode(token.token, { complete: true });
 
     expect(decoded).toHaveProperty('payload.grants.identity', token.identity);
     expect(decoded).toHaveProperty('payload.grants.chat.service_sid');
-    expect(decoded.payload.grants.chat.service_sid).toEqual('ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-    expect(decoded).toHaveProperty('payload.grants.data_sync.service_sid');
-    expect(decoded.payload.grants.data_sync.service_sid).toEqual("something456")
+    expect(decoded.payload.grants.chat.service_sid).toEqual(
+      config.TWILIO_CHAT_SERVICE_SID
+    );
   });
 
   it('generates a new token using the specified identity', () => {
-    const identity = "Alice";
+    const identity = 'Alice';
     const token = tokenGenerator(identity);
-    const decoded = jwt.decode(token.token, {complete: true});
+    const decoded = jwt.decode(token.token, { complete: true });
 
     expect(token.identity).toEqual(identity);
     expect(decoded).toHaveProperty('payload.grants.identity', token.identity);
   });
-
-
 });
