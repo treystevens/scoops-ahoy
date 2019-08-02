28391d1326b847c03930f9e967b840c457f60617
treykris <trey10027@gmail.com>
2019-08-02 09:15:03 -0400

    Prettier format

--- src/notification_handler.js
+++ src/notification_handler.js
@@ -10,28 +10,31 @@ const config = require('./config');
  * @return {Promise}
  *         {Object.status}
  *         {Object.data}
- *                {Object.message}
+ *         {Object.message}
  */
 exports.registerBind = function registerBind(binding) {
   const service = getTwilioClient();
 
-  return service.bindings.create(binding).then((binding) => {
-    console.log(binding);
-    // Send a JSON response indicating success
-    return {
-      status: 200,
-      data: {message: 'Binding created!'},
-    };
-  }).catch((error) => {
-    console.log(error);
-    return {
-      status: 500,
-      data: {
-        error: error,
-        message: 'Failed to create binding: ' + error,
-      },
-    };
-  });
+  return service.bindings
+    .create(binding)
+    .then(binding => {
+      console.log(binding);
+      // Send a JSON response indicating success
+      return {
+        status: 200,
+        data: { message: 'Binding created!' }
+      };
+    })
+    .catch(error => {
+      console.log(error);
+      return {
+        status: 500,
+        data: {
+          error: error,
+          message: 'Failed to create binding: ' + error
+        }
+      };
+    });
 };
 
 // Notify - send a notification from a POST HTTP request
@@ -40,28 +43,29 @@ exports.sendNotification = function sendNotification(notification) {
   const service = getTwilioClient();
 
   // Send a notification
-  return service.notifications.create(notification).then((message) => {
-    console.log(message);
-    return {
-      status: 200,
-      data: {message: 'Successful sending notification'},
-    };
-  }).catch((error) => {
-    console.log(error);
-    return {
-      status: 500,
-      data: {error: error},
-    };
-  });
+  return service.notifications
+    .create(notification)
+    .then(message => {
+      console.log(message);
+      return {
+        status: 200,
+        data: { message: 'Successful sending notification' }
+      };
+    })
+    .catch(error => {
+      console.log(error);
+      return {
+        status: 500,
+        data: { error: error }
+      };
+    });
 };
 
 function getTwilioClient() {
   // Twilio Library
-  const client = new Twilio(
-    config.TWILIO_API_KEY,
-    config.TWILIO_API_SECRET,
-    {accountSid: config.TWILIO_ACCOUNT_SID}
-  );
+  const client = new Twilio(config.TWILIO_API_KEY, config.TWILIO_API_SECRET, {
+    accountSid: config.TWILIO_ACCOUNT_SID
+  });
 
   // Get a reference to the user notification service instance
   const service = client.notify.services(
